6b1aeb3fb1cbeca867e7cc3a9862fcf9a7ec9472
Marcin Jasinski <marcin.jasinski@eo.pl>
2016-07-09 20:43:33 +0200

    init web project

---
+++ frontend-app/SharpGCalendar.Web/app/app.module.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('mwl.calendar.docs')
+        .factory('alert', function ($uibModal) {
+
+            function show(action, event) {
+                return $uibModal.open({
+                    templateUrl: 'modalContent.html',
+                    controller: function () {
+                        var vm = this;
+                        vm.action = action;
+                        vm.event = event;
+                    },
+                    controllerAs: 'vm'
+                });
+            }
+
+            return {
+                show: show
+            };
+
+        });
+
+})();
